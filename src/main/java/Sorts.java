fb710d6ca7974fb5e710a6575f8c2256b2a2dc37
Elioenai Roberto <42978220+LeoniSZ@users.noreply.github.com>
2021-05-06 16:13:33 -0300

    Update Sorts.java

--- src/main/java/Sorts.java
+++ src/main/java/Sorts.java
@@ -1,17 +1,22 @@
 
-import java.io.IOException;
 import java.io.*;
 import java.awt.GraphicsEnvironment;
-
+//import java.net.URISyntaxException;
 public class Sorts {
+    
+    public static void criarDiretorio(){
+        String name = "C:\\users";
+        new File(name).mkdir();
+    }
 
     public static void main(String[] args) throws IOException {
         
-        Console console = System.console();
+        criarDiretorio();
+    	Console console = System.console();
         if(console == null && !GraphicsEnvironment.isHeadless()){
             //System.out.println(filename);
-            //trocar o direitorio
-            String filename = "C:\\Users\\leoni\\sorts.jar";
+        	
+            String filename = "C:\\sorts\\sorts.jar";
             //System.out.println(filename);
             Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \""  + filename + "\""});
         }
